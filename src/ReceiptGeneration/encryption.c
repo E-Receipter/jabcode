a9c6392ee1bdc7132651202a711001fbc05c9ea4
dravog7 <dravog77@gmail.com>
2021-03-16 20:41:43 +0530

    documentation

--- src/ReceiptGeneration/encryption.c
+++ src/ReceiptGeneration/encryption.c
@@ -1,30 +1,38 @@
-/*
+/**
 Encryption and Decryption functions
 
 (Read jabcode.h in src/jabcode to understand jab_data)
-*/
+**/
 
 #include "jabcode.h"
 #include "encryption.h"
 
-/*
-@brief encrypt data using given key
-*/
+/**
+* @brief encrypt data using given key
+* @param key the private key for encryption
+* @param data the data to be encrypted
+* @return the encrypted data
+**/
 jab_data* RG_encrypt(jab_data* key,jab_data* data){
     // Encrypt enc_data -> data using enc_data -> key
     return data;
 }
 
-/*
-@brief decrypt data using the given key
-*/
+/**
+* @brief decrypt data using the given key
+* @param key the public key for decryption
+* @param data the data to be decrypted
+* @return the decrypted data
+**/
 jab_data* RG_decrypt(jab_data* key,jab_data* data){
     return data;
 }
 
-/*
-@brief generate a signature for the given data
-*/
+/**
+* @brief generate a signature for the given data
+* @param data the data whose signature is to be found
+* @return the signature
+**/
 jab_data* RG_sign(jab_data* data){
     return data;
 }
--- src/ReceiptGeneration/jab_enc.c
+++ src/ReceiptGeneration/jab_enc.c
@@ -5,6 +5,11 @@ JABCode encoding and decoding functions
 #include "jabcode.h"
 #include "jab_enc.h"
 
+/**
+ * @brief JABCode Bitmap decoding function
+ * @param bitmap the bitmap of the image to be processed
+ * @return the decoded data and status
+**/
 jab_decode* RG_decode(jab_bitmap *bitmap){
     jab_int32 decode_status;
 	jab_decoded_symbol symbols[MAX_SYMBOL_NUMBER];
